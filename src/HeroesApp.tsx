2ef05705874067037070110a812e356a62d405cd
CarlosArancibia <arancibiacarlos.b@gmail.com>
2024-01-11 23:54:39 -0400

    pages added

--- src/HeroesApp.tsx
+++ src/HeroesApp.tsx
@@ -1,5 +1,7 @@
-import React from 'react';
-
 export const HeroesApp = () => {
-  return <div>HeroesApp</div>;
+  return (
+    <>
+      <h1 className="text-3xl font-bold">HeroesApp</h1>
+    </>
+  );
 };
