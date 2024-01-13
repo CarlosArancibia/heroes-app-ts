3c2455b0101da720678d38366e34ef7b0009579c
CarlosArancibia <arancibiacarlos.b@gmail.com>
2024-01-12 21:36:29 -0400

    fix searches

--- src/heroes/components/getHeroesByName.ts
+++ src/heroes/components/getHeroesByName.ts
@@ -3,5 +3,7 @@ import { heroes } from '../data/heroes';
 export const getHeroesByName = (name: string) => {
   name = name.toLowerCase().trim();
 
+  if (name.length <= 1) return [];
+
   return heroes.filter(({ superhero }) => superhero.toLowerCase().includes(name));
 };
--- src/heroes/pages/SearchPage.tsx
+++ src/heroes/pages/SearchPage.tsx
@@ -8,6 +8,9 @@ export const SearchPage = () => {
   const q = search.replace('?q=', '');
   const heroes = getHeroesByName(q);
 
+  const showSearch = q.length === 0;
+  const showEmpty = q.length > 0 && heroes.length === 0;
+
   return (
     <section className="max-w-[1400px] mx-auto p-10 flex gap-24">
       <section className="flex-1">
@@ -19,6 +22,9 @@ export const SearchPage = () => {
         <h2 className="text-2xl">Results</h2>
         <hr className="mt-1 mb-5" />
         <div className="flex flex-col gap-3">
+          {showSearch && 'Waiting to search'}
+          {showEmpty && 'No heroes'}
+
           {heroes.map((hero) => (
             <HeroCard key={hero.id} {...hero} />
           ))}
